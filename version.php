9e763ba7c0b281434c2a371676dbb1bbac4e5223
Guenther Moser <moser@tugraz.at>
2023-09-11 10:56:38 +0200

    new tag

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2023091100; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700; // Requires this Moodle version 3.11.
 $plugin->component = 'lytix_logs'; // Full name of the plugin.
-$plugin->release   = 'v1.0.3';
+$plugin->release   = 'v1.0.4';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->supported = [401, 401];
